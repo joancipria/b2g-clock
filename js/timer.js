6fdf68a05dccc5582cbba84484f0bfeb45b85853
joancipria <joancipria@gmail.com>
2016-10-23 01:33:09 +0200

    Solved: ReferenceError: audio is not defined when pressing "Reset" button on timer.

--- js/timer.js
+++ js/timer.js
@@ -1,132 +1,132 @@
+var audio = new Audio('sounds/ac_awake.opus.ogx');
+audio.loop = true;
+
 function StartCountDown() {
-document.getElementById('count-down').style.display="block";
-document.getElementById('input-layer').style.display="none";
-document.getElementById('countdown').style.color="#cfe2e6";
+    document.getElementById('count-down').style.display = "block";
+    document.getElementById('input-layer').style.display = "none";
+    document.getElementById('countdown').style.color = "#cfe2e6";
 
-var CountDown = (function ($) {
-
-
-    // Length ms
-    var TimeOut = 100000;
-    // Interval ms
-    var TimeGap = 1000;
-
-    var CurrentTime = ( new Date() ).getTime();
-    var EndTime = ( new Date() ).getTime() + TimeOut;
+    var CountDown = (function($) {
 
 
-    var GuiTimer = $('#countdown');
-    var GuiPause = $('#pause');
-    var GuiResume = $('#resume').hide();
+        // Length ms
+        var TimeOut = 100000;
+        // Interval ms
+        var TimeGap = 1000;
 
-    var wantReset = false;
-    var Running = true;
+        var CurrentTime = (new Date()).getTime();
+        var EndTime = (new Date()).getTime() + TimeOut;
 
-    var UpdateTimer = function() {
-        // Run till timeout
-        if( CurrentTime + TimeGap < EndTime ) {
-            setTimeout( UpdateTimer, TimeGap );
-        }
-        // Countdown if running
-        if( Running ) {
-            CurrentTime += TimeGap;
-            if( CurrentTime >= EndTime && wantReset==true) {
-    					 GuiPause.hide();
+
+        var GuiTimer = $('#countdown');
+        var GuiPause = $('#pause');
+        var GuiResume = $('#resume').hide();
+
+        var wantReset = false;
+        var Running = true;
+
+        var UpdateTimer = function() {
+            // Run till timeout
+            if (CurrentTime + TimeGap < EndTime) {
+                setTimeout(UpdateTimer, TimeGap);
+            }
+            // Countdown if running
+            if (Running) {
+                CurrentTime += TimeGap;
+                if (CurrentTime >= EndTime && wantReset == true) {
+                    GuiPause.hide();
+                }
+
+                if (CurrentTime >= EndTime && wantReset == false) {
+                    GuiTimer.css('color', 'red');
+                    audio.play();
+                    GuiPause.hide();
+                }
+
+            }
+            // Update Gui
+            var Time = new Date();
+            Time.setTime(EndTime - CurrentTime);
+            var Hours = Time.getHours();
+            var Minutes = Time.getMinutes();
+            var Seconds = Time.getSeconds();
+
+
+            if (document.getElementById('input-time-3').value == "" || document.getElementById('input-time-3').value == "1" && document.getElementById('input-time-2').value == "00" && document.getElementById('input-time').value == "00" || document.getElementById('input-time-3').value == "01" && document.getElementById('input-time-2').value == "00" && document.getElementById('input-time').value == "00") {
+
+                GuiTimer.html(
+                    (Minutes < 10 ? '0' : '') + Minutes +
+                    ':' +
+                    (Seconds < 10 ? '0' : '') + Seconds)
+            } else {
+
+                GuiTimer.html(
+                    (Hours < 10 ? '0' : '') + Hours +
+                    ':' +
+                    (Minutes < 10 ? '0' : '') + Minutes +
+                    ':' +
+                    (Seconds < 10 ? '0' : '') + Seconds);
             }
 
-			if( CurrentTime >= EndTime && wantReset==false) {
-                GuiTimer.css('color','red');
-                audio = new Audio('sounds/ac_awake.opus.ogx');
-								audio.loop=true;
-					 audio.play();
-					 GuiPause.hide();
-            }
+        };
 
-        }
-        // Update Gui
-        var Time = new Date();
-        Time.setTime( EndTime - CurrentTime );
-        var Hours = Time.getHours();
-        var Minutes = Time.getMinutes();
-        var Seconds = Time.getSeconds();
+        var Pause = function() {
+            Running = false;
+            GuiPause.hide();
+            GuiResume.show();
+        };
 
 
-if (document.getElementById('input-time-3').value=="" || document.getElementById('input-time-3').value=="1" && document.getElementById('input-time-2').value=="00" && document.getElementById('input-time').value=="00" || document.getElementById('input-time-3').value=="01" && document.getElementById('input-time-2').value=="00" && document.getElementById('input-time').value=="00" ) {
+        var Resume = function() {
+            Running = true;
+            GuiPause.show();
+            GuiResume.hide();
+        };
 
-        GuiTimer.html(
-         (Minutes < 10 ? '0' : '') + Minutes
-            + ':'
-            + (Seconds < 10 ? '0' : '') + Seconds)}
+        var Reset = function() {
+            EndTime = 0;
+            document.getElementById('count-down').style.display = "none";
+            document.getElementById('input-layer').style.display = "block";
+            wantReset = true;
+            audio.pause();
+            clearX();
+        };
 
-
-           else {
-
-        GuiTimer.html(
-            (Hours < 10 ? '0' : '') + Hours
-            + ':'
-            + (Minutes < 10 ? '0' : '') + Minutes
-            + ':'
-            + (Seconds < 10 ? '0' : '') + Seconds);
-           }
-
-    };
-
-    var Pause = function() {
-        Running = false;
-        GuiPause.hide();
-        GuiResume.show();
-    };
+        var Start = function(Timeout) {
+            TimeOut = Timeout;
+            CurrentTime = (new Date()).getTime();
+            EndTime = (new Date()).getTime() + TimeOut;
+            UpdateTimer();
+            wantReset = false;
+            Running = true;
+            GuiPause.show();
 
 
-    var Resume = function() {
-        Running = true;
-        GuiPause.show();
-        GuiResume.hide();
-    };
+        };
 
-var Reset = function() {
-EndTime=0;
-document.getElementById('count-down').style.display="none";
-document.getElementById('input-layer').style.display="block";
-wantReset=true;
-audio.pause();
-clearX();
-    };
-
-    var Start = function( Timeout ) {
-        TimeOut = Timeout;
-        CurrentTime = ( new Date() ).getTime();
-        EndTime = ( new Date() ).getTime() + TimeOut;
-        UpdateTimer();
-        wantReset=false;
-        Running = true;
-        GuiPause.show();
+        return {
+            Pause: Pause,
+            Resume: Resume,
+            Start: Start,
+            Reset: Reset
+        };
 
 
-    };
+    })(jQuery);
 
-    return {
-        Pause: Pause,
-        Resume: Resume,
-        Start: Start,
-        Reset: Reset
-    };
+    jQuery('#pause').on('click', CountDown.Pause);
+    jQuery('#resume').on('click', CountDown.Resume);
+    jQuery('#reset').on('click', CountDown.Reset);
 
+    var Input3 = (((document.getElementById('input-time-3').value) * 60) * 60) * 1000;
+    var Input2 = ((document.getElementById('input-time-2').value) * 60) * 1000;
+    var Input1 = (document.getElementById('input-time').value) * 1000;
+    var totalTime = parseInt(Input1) + parseInt(Input2) + parseInt(Input3);
 
-})(jQuery);
-
-jQuery('#pause').on('click',CountDown.Pause);
-jQuery('#resume').on('click',CountDown.Resume);
-jQuery('#reset').on('click',CountDown.Reset);
-
-var Input3 = (((document.getElementById('input-time-3').value)*60)*60)*1000;
-var Input2 = ((document.getElementById('input-time-2').value)*60)*1000;
-var Input1 = (document.getElementById('input-time').value)*1000;
-
-if (document.getElementById('input-time-3').value=="" || document.getElementById('input-time-3').value=="1" && document.getElementById('input-time-2').value=="00" && document.getElementById('input-time').value=="00" || document.getElementById('input-time-3').value=="01" && document.getElementById('input-time-2').value=="00" && document.getElementById('input-time').value=="00" ) {
-var finalTime = parseInt(Input1)+parseInt(Input2)+parseInt(Input3);
-}else {
-var finalTime = (parseInt(Input1)+parseInt(Input2)+parseInt(Input3)-3600000);
-}
-CountDown.Start(finalTime);
+    if (document.getElementById('input-time-3').value == "" || document.getElementById('input-time-3').value == "1" && document.getElementById('input-time-2').value == "00" && document.getElementById('input-time').value == "00" || document.getElementById('input-time-3').value == "01" && document.getElementById('input-time-2').value == "00" && document.getElementById('input-time').value == "00") {
+        var finalTime = parseInt(Input1) + parseInt(Input2) + parseInt(Input3);
+    } else {
+        var finalTime = (parseInt(Input1) + parseInt(Input2) + parseInt(Input3) - 3600000);
+    }
+    CountDown.Start(finalTime);
 }
